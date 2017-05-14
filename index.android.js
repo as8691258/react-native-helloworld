14da0c07baf0ac457c7f2097617373867c063cc3
liu-885@163.com <258464354>
2017-05-14 23:11:39 +0800

    2017.05.14创建项目

---
+++ index.android.js
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+var Swiper = require('react-native-swiper');
+
+var sliderImgs = [
+    'http://images3.c-ctrip.com/SBU/apph5/201505/16/app_home_ad16_640_128.png',
+    'http://images3.c-ctrip.com/rk/apph5/C1/201505/app_home_ad49_640_128.png',
+    'http://images3.c-ctrip.com/rk/apph5/D1/201506/app_home_ad05_640_128.jpg'
+];
+
+export default class HelloWorld extends Component {
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text >
+          首页
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  }
+});
+
+AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
